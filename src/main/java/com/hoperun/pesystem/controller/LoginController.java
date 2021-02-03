087d037d846a4f276c4178c17ca3210baadf842e
ljd <1264025221@qq.com>
2021-02-03 18:54:43 +0800

    登录check信息码

--- src/main/java/com/hoperun/pesystem/controller/LoginController.java
+++ src/main/java/com/hoperun/pesystem/controller/LoginController.java
@@ -1,12 +1,9 @@
 package com.hoperun.pesystem.controller;
-
-import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hoperun.pesystem.dto.ResultDto;
 import com.hoperun.pesystem.exception.CustomizeErrorCode;
-import com.hoperun.pesystem.model.User;
 import com.hoperun.pesystem.service.LoginService;
 import com.hoperun.pesystem.utils.TokenUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -19,24 +16,15 @@ public class LoginController {
     private LoginService loginService;
         @PostMapping("/login")
         @ResponseBody
-        public ResultDto<?> login(@RequestBody JSONObject jsonObject) throws JsonProcessingException {
-
-            String phonenumber=jsonObject.getString("loginName");
-            String password=jsonObject.getString("password");
-
-//            String phonenumber=(String)para.get("phonenumber");
-//            String password=(String)para.get("password");
-            if (loginService.checkLogin(phonenumber,password)){
-//                user.setUserPassword(password);
-                String token= TokenUtils.sign(phonenumber);
-//                HashMap<String,Object> hs=new HashMap<>();
-//                hs.put("token",token+phonenumber+password);
-//                ObjectMapper objectMapper=new ObjectMapper();
-//                return objectMapper.writeValueAsString(hs);
-               return  ResultDto.LoginOK(token);
+        public ResultDto<?> login(  @RequestParam("phonenumber") String phonenumber,
+                                    @RequestParam("password") String password ){
+            if (StringUtils.isNotBlank(phonenumber) && StringUtils.isNotBlank(password)) {
+                if (loginService.checkLogin(phonenumber,password)){
+                    String token= TokenUtils.sign(phonenumber);
+                    return  ResultDto.LoginOK(token);
+                }
+                return  ResultDto.errorOf(CustomizeErrorCode.LOGIN_FAILED);
             }
-            return  ResultDto.errorOf(CustomizeErrorCode.LOGIN_FAILED);
+            return  ResultDto.errorOf(CustomizeErrorCode.LOGIN_ISBLACK);
         }
-
-
     }
--- src/main/java/com/hoperun/pesystem/exception/CustomizeErrorCode.java
+++ src/main/java/com/hoperun/pesystem/exception/CustomizeErrorCode.java
@@ -3,6 +3,7 @@ package com.hoperun.pesystem.exception;
 
 public enum CustomizeErrorCode implements ICustomizeErrorCode {
     LOGIN_FAILED(4001, "登录失败，密码或账号错误！"),
+    LOGIN_ISBLACK(4001, "手机号或密码不能为空！"),
     REGISTER_FAILED(4002, "该手机号码已经注册！"),
     INTEGRAL_NOT_ENOUGH(4003, "兑换失败，积分不足！"),
 
